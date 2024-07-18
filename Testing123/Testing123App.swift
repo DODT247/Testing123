3538db2fea90df306594df8ed4c2243dfb94313b
Helloadil <hello.adil.ali@gmail.com>
2024-07-18 18:49:19 +0200

    Initial Commit

---
+++ Testing123/Testing123App.swift
@@ -0,0 +1,17 @@
+//
+//  Testing123App.swift
+//  Testing123
+//
+//  Created by Mr Ali on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Testing123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
